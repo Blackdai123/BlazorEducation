48238e646ea92ea9933cb047bd84fe9ee5be83c5
Blackdai123 <104855827+Blackdai123@users.noreply.github.com>
2023-07-14 19:03:17 +0300

    myFIrstCommit

---
+++ MyFirstBlazorApp/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace MyFirstBlazorApp.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleseDate { get; set; }
+
+        public string URLLogo { get; set; }
+        public string URLGame { get; set; }
+
+    }
+}
